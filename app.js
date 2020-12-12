27e6ea54a441a996048e48e8f33b10b82a9b6e36
THANH HIEN NGUYEN <tphcm.hiendeptrai@gmail.com>
2020-12-12 09:21:55 +0700

    them app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const server = require("express");
+const app = server();
+
+// lay router tu file index.js dax export ra, ko can link toi index.js thi no auto link
+const userRouter = require('./src/user/');
+
+const port = 8069;
+
+app.use(server.json());
+
+app.use('/api', userRouter);
+
+app.listen(port);
